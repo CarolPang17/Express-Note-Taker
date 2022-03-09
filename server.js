1c8f5a58bfad15a9ccdb7ae3d09daf033a0560aa
carolpang <carolpang17@gmail.com>
2022-03-08 23:39:41 -0800

    setting up app get and listen

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Require express and files
 const express = require("express");
 const fs = require("fs");
+const db = require("./db/db.json");
+var path = require("path");
 //Require routes file
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Initialize express app
+// // Initialize express app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,9 +16,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
+// require("./routes")
+
+app.get("/", function (req, res) {
+    console.log('we got the first get')
+    res.send("");
+});
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+})
 
 // Setup listener
 app.listen(PORT, function() {
