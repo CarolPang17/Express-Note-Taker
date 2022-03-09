1697f30b6b6fc81976a3172aecc6d941dbf2700a
carolpang <carolpang17@gmail.com>
2022-03-09 09:52:12 -0800

    get and post feature sucessfully on

--- server.js
+++ server.js
@@ -1,36 +1,64 @@
 // Require express and files
-const express = require("express");
-const fs = require("fs");
-const db = require("./db/db.json");
+var express = require("express");
+var fs = require("fs");
+var notesData = require("./db/db.json");
 var path = require("path");
-//Require routes file
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
 // // Initialize express app
-const PORT = process.env.PORT || 3001;
-const app = express();
+var PORT = process.env.PORT || 3001;
+var app = express();
 
 // adding middleware function for parse incomingstring or array data / JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
 
-// require("./routes")
-
-app.get("/", function (req, res) {
-    console.log('we got the first get')
-    res.send("");
-});
+var currentID = notesData.length;
+var newNote;
 
 app.get("/api/notes", function (req, res) {
-    return res.json(notes);
+    return res.json(notesData);
 })
+
+
+app.post("/api/notes", function (req, res) {
+    newNote = req.body;
+
+    currentID++;
+
+    newNote["id"] = currentID;
+
+    notesData.push(newNote);
+
+    updateNotesData();
+
+    return res.status(200).end();
+});
+
+
+//html routes
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 
 // Setup listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
+function updateNotesData() {
+    fs.writeFile("./db/db.json", JSON.stringify(notesData), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log(`New new note ${JSON.stringify(newNote)} has been added to notesData`);
+    });
+}
